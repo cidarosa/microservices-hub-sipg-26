98066ca61ec86e214767e6fbef39e2689bb8e909
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-08 10:29:31 -0300

    feat: update import.sql, add findAll PagamentoControllerIT

---
+++ ms-pagamentos/src/test/java/com/github/cidarosa/ms_pagamentos/controller/PagamentoControllerIT.java
@@ -0,0 +1,54 @@
+package com.github.cidarosa.ms_pagamentos.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.cidarosa.ms_pagamentos.entities.Pagamento;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+public class PagamentoControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Pagamento pagamento;
+    private Long existingId;
+    private Long nonExistingId;
+
+    @BeforeEach
+    void setUp() {
+        existingId = 1L;
+        nonExistingId = Long.MAX_VALUE;
+    }
+
+    @Test
+    void findAllPagamentosShouldReturn200AndJsonArray() throws Exception {
+
+        mockMvc.perform(get("/pagamentos")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect(jsonPath("$[*].id").isArray())
+                .andExpect(jsonPath("$[1].valor").value(3599.0));
+    }
+}
